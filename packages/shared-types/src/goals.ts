cd14aaae3879e6ac721a4f118ac7150d4daa3fde
Anik <contact.anik.apps+fynans@gmail.com>
2026-04-17 11:59:36 -0700

    feat: shared savings-goal types + constants

---
+++ packages/shared-types/src/goals.ts
@@ -0,0 +1,5 @@
+export type PaceStatus = "ahead" | "on_pace" | "behind" | "target_passed";
+export type GoalStatus = "active" | "completed" | "archived";
+
+export const MAX_NAME_LENGTH = 255;
+export const MAX_NOTES_LENGTH = 500;
